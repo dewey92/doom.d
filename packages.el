2b411a69599bf2bd16705f64a57849be1ec06097
Jihad D. Waspada <dewey992@gmail.com>
2019-05-01 12:13:04 +0200

    first

---
+++ packages.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; -*-
+;;; ~/.doom.d/packages.el
+
+(package! flycheck)
+(package! org)
+(package! lsp-mode)
+(package! lsp-ui :recipe (:fetcher github :repo "emacs-lsp/lsp-ui"))
+(package! lsp-haskell)
+(package! haskell-mode)
+(package! flycheck-haskell)
+(package! company-lsp)
