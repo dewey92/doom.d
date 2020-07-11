2e8578f081bbf9e777008bac58c9f4c04437f8fa
Jihad D. Waspada <dewey992@gmail.com>
2020-07-11 22:29:26 +0200

    Fix indentation

--- config.el
+++ config.el
@@ -110,7 +110,10 @@
   (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
   (setq tide-hl-identifier-idle-time 0.2)
-  (setq web-mode-enable-current-element-highlight t))
+  (setq web-mode-enable-current-element-highlight t)
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-markup-indent-offset 2)
+  (setq typescript-indent-level 2))
 
 (add-hook 'web-mode-hook #'tsx-setup-tide)
 
